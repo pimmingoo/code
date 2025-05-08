82f2c4153470542574e78624039d15c316a75539
pimmingoo <pmprins2007@gmail.com>
2025-05-08 11:09:47 +0200

    sexy

---
+++ NodejsPimPrins/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+import chalk from "chalk";
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+res.send("Hallo vanaf je sinterklaas Node-server!");
+});
+
+app.listen(PORT, () => {
+console.log(chalk.green(`Server draait op http://localhost:${PORT}`));
+});
