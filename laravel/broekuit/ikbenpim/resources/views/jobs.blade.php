ed4b886f7257073ac998440817043c2963cf3528
pimmingoo <pmprins2007@gmail.com>
2026-04-02 14:17:49 +0200

    laravel les 13

--- laravel/broekuit/ikbenpim/resources/views/jobs.blade.php
+++ laravel/broekuit/ikbenpim/resources/views/jobs.blade.php
@@ -3,11 +3,16 @@
         Job page
     </x-slot:heading>
 
+    <div class="space-y-4">
     @foreach ($jobs as $job)
-        <li>
-            <a href="/jobs/{{ $job['id'] }}" class="hover:underline">
+        <a href="/jobs/{{ $job['id'] }}" class="block px-4 py-6 border-2 border-gray-500 rounded-lg hover:border-opacity-25">
+            <div class="font-bold text-blue-500 text-sm">
+                {{ $job->employer->name }}
+            </div>
+            <div>
                 <strong>{{ $job['title'] }}</strong> - Pays: {{ $job['salary'] }} Per Year
-            </a>
-        </li>
+            </div>
+        </a>
     @endforeach
+    </div>
 </x-layout>
