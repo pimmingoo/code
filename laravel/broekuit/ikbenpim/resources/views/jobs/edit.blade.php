49b244ec189585459e7645cc5c1b4b4c3e12e0e4
pimmingoo <pmprins2007@gmail.com>
2026-04-09 12:25:11 +0200

    les 18

---
+++ laravel/broekuit/ikbenpim/resources/views/jobs/edit.blade.php
@@ -0,0 +1,71 @@
+<x-layout>
+    <x-slot:heading>
+        Edit Job: {{ $job->title }}
+    </x-slot:heading>
+
+    <form method="POST" action="/jobs/{{ $job->id }}">
+        @csrf
+        @method('PATCH')
+
+        <div class="space-y-12">
+            <div class="border-b border-gray-900/10 pb-12">
+
+                <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                    <div class="sm:col-span-4">
+                        <label for="title" class="block text-sm/6 font-medium text-gray-900">Title</label>
+                        <div class="mt-2">
+                            <div class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
+                                <input required value="{{ $job->title }}" id="title" type="text" name="title" placeholder="shift leader" class="block min-w-0 grow bg-white py-1.5 px-3 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" />
+                            </div>
+
+                            @error('title')
+                                <p class="text-xs text-red-500 font-semibold mt-3"> {{ $message }} </p>
+                            @enderror
+                        </div>
+                    </div>
+                </div>
+
+                <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                    <div class="sm:col-span-4">
+                        <label for="salary" class="block text-sm/6 font-medium text-gray-900">Salary</label>
+                        <div class="mt-2">
+                            <div class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
+                                <input required value="{{ $job->salary }}" id="salary" type="text" name="salary" placeholder="€50.000 Per Year" class="block min-w-0 grow bg-white py-1.5 px-3 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" />
+                            </div>
+
+                            @error('salary')
+                                <p class="text-xs text-red-500 font-semibold mt-3"> {{ $message }} </p>
+                            @enderror
+                        </div>
+                    </div>
+                </div>
+
+                {{-- <div class="mt-10">
+                    @if ($errors->any())
+                        <ul>
+                            @foreach($errors->all() as $error)
+                            <li class="text-red-500 italic"> {{ $error }} </li>
+                            @endforeach
+                        </ul>
+                    @endif
+                </div> --}}
+            </div>
+
+            <div class="mt-6 flex items-center justify-between gap-x-6">
+                <div>
+                    <button form="delete-form" class="text-red-500 text-sm font-bold">Delete</button>
+                </div>
+
+                <div class="flex-wrap space-x-3">
+                    <a href="/jobs/{{ $job->id }}" type="button" class="text-sm/6 font-semibold text-gray-900">Cancel</a>
+                    <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Update</button>
+                </div>
+            </div>
+        </div>
+    </form>
+
+    <form class="hidden" id="delete-form" method="POST" action="/jobs/{{ $job->id }}">
+        @csrf
+        @method('DELETE')
+    </form>
+</x-layout>
--- laravel/broekuit/ikbenpim/resources/views/jobs/show.blade.php
+++ laravel/broekuit/ikbenpim/resources/views/jobs/show.blade.php
@@ -3,9 +3,13 @@
         Job
     </x-slot:heading>
 
-    <h2 class="font-bold text-lg">{{ $job['title'] }}</h2>
+    <h2 class="font-bold text-lg">{{ $job->title }}</h2>
 
     <p>
-        This Job Pays {{ $job['salary'] }} per year.
+        This Job Pays {{ $job->salary }} per year.
     </p>
+
+    <div class="mt-6">
+        <x-button href="/jobs/{{ $job->id }}/edit"> Edit Job </x-button>
+    </div>
 </x-layout>
